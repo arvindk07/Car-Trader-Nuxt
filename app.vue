b933c1b6965d6dc68160db9d5f01b00dfcc5afab
arvindk07 <info.arvind07@gmail.com>
2023-04-17 12:55:39 +0530

    header

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- Navbar -->
+    <header
+      class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
+    >
+      <a class="text-2xl" href=""> carTrader</a>
+    </header>
+    <!-- Navbar -->
   </div>
 </template>
