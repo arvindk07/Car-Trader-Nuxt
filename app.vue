2f952984097b274b6c5c5b719eb3f82f064d9ff6
arvindk07 <info.arvind07@gmail.com>
2023-04-17 13:37:31 +0530

    home hero

--- app.vue
+++ app.vue
@@ -7,5 +7,18 @@
       <a class="text-2xl" href=""> carTrader</a>
     </header>
     <!-- Navbar -->
+
+    <!-- home hero -->
+    <div
+      class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://r4.wallpaperflare.com/wallpaper/590/199/595/auto-machine-nissan-nfs-need-for-speed-hd-wallpaper-69e88ca006419fb4bba25b4e2cf329ec.jpg')]"
+    >
+      <div class="z-10 absolute w-full h-full flex justify-center items-center">
+        <div class="text-center w-1/2">
+          <h1 class="text-8xl font-serif mb-10 text-white">Find Your Drive</h1>
+        </div>
+      </div>
+      <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
+    </div>
+    <!-- home hero -->
   </div>
 </template>
