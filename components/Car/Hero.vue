c5826f9db03c558e308332fac9af02b8eea25561
arvindk07 <info.arvind07@gmail.com>
2023-04-18 08:04:33 +0530

    vue components

---
+++ components/Car/Hero.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://r4.wallpaperflare.com/wallpaper/590/199/595/auto-machine-nissan-nfs-need-for-speed-hd-wallpaper-69e88ca006419fb4bba25b4e2cf329ec.jpg')]"
+  >
+    <div class="z-10 absolute w-full h-full flex justify-center items-center">
+      <div class="text-center w-100">
+        <h1 class="text-8xl font-serif mb-10 text-white">Find Your Drive</h1>
+        <!-- HOME SEARCH BAR -->
+        <CarSearchBar />
+        <!-- HOME SEARCH BAR -->
+      </div>
+    </div>
+    <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+import Hero from '~/components/Car/Hero.vue';
+
+<template>
+  <div>
+    <NavBar />
+    <CarHero />
+  </div>
+</template>
